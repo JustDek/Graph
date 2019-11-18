dadf3e13c89c256dbae4fe3b3bc0ae65acdd252f
denis.vivdenko.freelancer@gmail.com <denis.vivdenko.freelancer@gmail.com>
2019-11-18 14:16:39 +0000

    Initial sturcture of graph method add adge (Adjacency List)

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Graph
 {
     class Graph
     {
+        Dictionary<int, List<int>> graph;
 
+        private List<bool> visited;
+        public Graph()
+        {
+            graph = new Dictionary<int, List<int>>();
+        }
 
+        public void AddEdge(int node, int edge)
+        {
+            if (!graph.ContainsKey(node))
+            {
+                graph.Add(node, new List<int>(){ edge });
+            } 
+            else
+            {
+                graph[node].Add(edge);
+            }
+        }
+
+        public void DFS()
+        {
+            visited = new List<bool>();
+            for (int i = 0; i < graph.Count(); i++)
+            {
+                visited.Add(false);
+            }
+            
+       
+        }
+
+        private void DFSfunc(int node)
+        {
+            visited[node] = true;
+
+            
+
+        }
     }
 }
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -6,7 +6,11 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            
+            Graph gr = new Graph();
+            gr.AddEdge(1, 2);
+            gr.AddEdge(1, 3);
+            gr.AddEdge(2, 3);
+            gr.DFS();
         }
     }
 }
