2eb0c6ab3da5fd1eef21f40114cfaf96cec7c7ce
denis.vivdenko.freelancer@gmail.com <denis.vivdenko.freelancer@gmail.com>
2019-11-20 18:17:03 +0000

    Write methods addNode and addEdge

--- Graph/Graphs/DirectedUnweightedGraph.cs
+++ Graph/Graphs/DirectedUnweightedGraph.cs
@@ -7,14 +7,35 @@ namespace Graph.Graphs
 {
     class DirectedUnweightedGraph<TNodeType> : IGraph<TNodeType>
     {
+        private List<TNodeType> nodes;
+        private readonly int len;
+        private Dictionary<TNodeType, List<TNodeType>> adjacencyList;
+
+        public DirectedUnweightedGraph(int len)
+        {
+            this.len = len;
+            adjacencyList = new Dictionary<TNodeType, List<TNodeType>>();
+            
+        }
+        
         public void AddEdge(TNodeType from, TNodeType to, int weight = 0)
         {
-            throw new NotImplementedException();
+            if ( !(adjacencyList.ContainsKey(from) && adjacencyList.ContainsKey(to)) )
+            {
+                throw new Exception("There is no such node");
+            }
+            else
+            {
+                adjacencyList[from].Add(to);
+            }
         }
 
         public void AddNode(TNodeType node)
         {
-            throw new NotImplementedException();
+            if (!adjacencyList.ContainsKey(node))
+            {
+                adjacencyList.Add(node, new List<TNodeType>());
+            }
         }
 
         public void RemoveEdge(TNodeType from, TNodeType to)
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -7,24 +7,16 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            DirectedUnweightedGraph<int> graph = new DirectedUnweightedGraph<int>();
+            DirectedUnweightedGraph<int> gr = new DirectedUnweightedGraph<int>(5);
 
-            Graph gr = new Graph();
+            for (int i = 0; i < 5; i++)
+            {
+                gr.AddNode(i);
+            }
+
             gr.AddEdge(0, 1);
-            gr.AddEdge(0, 2);
-            gr.AddEdge(1, 0);
-            gr.AddEdge(1, 2);
-            gr.AddEdge(2, 0);
-            gr.AddEdge(2, 1);
-            gr.AddEdge(2, 3);
-            gr.AddEdge(2, 5);
-            gr.AddEdge(3, 2);
-            gr.AddEdge(3, 4);
-            gr.AddEdge(5, 2);
-            gr.AddEdge(5, 8);
-            gr.AddEdge(5, 6);
-
-            gr.DFS(0);
+            gr.AddEdge(1, 3);
+            gr.AddEdge(1, 5);
         }
     }
 }
