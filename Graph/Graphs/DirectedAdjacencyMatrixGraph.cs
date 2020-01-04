fd2ed846814615e4143c63501632ac39835d4801
denis.vivdenko.freelancer@gmail.com <denis.vivdenko.freelancer@gmail.com>
2020-01-04 10:44:30 +0000

    added algorithm to find max flow of network (Ford-Fulkerson)

--- Graph/Graphs/DirectedAdjacencyMatrixGraph.cs
+++ Graph/Graphs/DirectedAdjacencyMatrixGraph.cs
@@ -76,5 +76,68 @@ namespace Graph.Graphs
             }
             return false;
         }
+
+        public bool BFS(int[,] residualGraph, int s, int t, int[] path)
+        {
+            bool[] visited = new bool[nodeNum];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(s);
+            visited[s] = true;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+                for (int nextNode = 0; nextNode < nodeNum; nextNode++)
+                {
+                    if (visited[nextNode] == false && residualGraph[node, nextNode] > 0)
+                    {
+                        visited[nextNode] = true;
+                        queue.Enqueue(nextNode);
+                        path[nextNode] = node;
+                    }
+                }
+
+            }
+
+            return (visited[t] == true);
+        }
+
+        public int MaxFlowFord()
+        {
+            int s = 0, t = nodeNum - 1;
+            int[,] residualGraph = new int[nodeNum, nodeNum];
+            for (int i = 0; i < nodeNum; i++)
+            {
+                for (int j = 0; j < nodeNum; j++)
+                {
+                    residualGraph[i, j] = adjacencyMatrix[i, j];
+                }
+            }
+
+            int[] path = new int[nodeNum];
+            int maxFlow = 0;
+
+            while (BFS(residualGraph, s, t, path))
+            {
+                int pathFlow = int.MaxValue;
+                for (int node = t; node != s; node = path[node])
+                {
+                    int prevNode = path[node];
+                    pathFlow = Math.Min(pathFlow, residualGraph[prevNode, node]);
+                }
+                
+                for (int node = t; node != s; node = path[node])
+                {
+                    int prevNode = path[node];
+                    residualGraph[prevNode, node] -= pathFlow;
+                    residualGraph[node, prevNode] += pathFlow;
+                }
+
+                maxFlow += pathFlow;
+            }
+
+            return maxFlow;
+        }
     }
 }
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -7,13 +7,18 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            DirectedAdjacencyMatrixGraph gr = new DirectedAdjacencyMatrixGraph(4);
-            gr.AddEdge(0, 1, 1);
-            gr.AddEdge(0, 2, 3);
-
-            gr.AddEdge(2, 3, 1);
-            gr.AddEdge(3, 2, 2);
-            gr.DFS(0);
+            DirectedAdjacencyMatrixGraph gr = new DirectedAdjacencyMatrixGraph(6);
+            gr.AddEdge(0, 1, 16);
+            gr.AddEdge(0, 2, 13);
+            gr.AddEdge(1, 2, 10);
+            gr.AddEdge(1, 3, 12);
+            gr.AddEdge(2, 1, 4);
+            gr.AddEdge(2, 4, 14);
+            gr.AddEdge(3, 2, 9);
+            gr.AddEdge(3, 5, 20);
+            gr.AddEdge(4, 3, 7);
+            gr.AddEdge(4, 5, 4);
+            Console.WriteLine(gr.MaxFlowFord());
         }
     }
 }
